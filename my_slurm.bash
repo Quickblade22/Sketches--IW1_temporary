27dbb48c879a813d6de792665c4fb62322696968
Aaditya Mehta <aaditya.mehta@ml.rwth-aachen.de>
2025-10-23 13:44:15 +0200

    second way

--- my_slurm.bash
+++ my_slurm.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks-per-node=1         # Number of tasks per node
-#SBATCH --cpus-per-task=16          # 16 CPU cores per task
-#SBATCH --mem=64g                   # 64GB memory allocation
+#SBATCH --cpus-per-task=32          # 16 CPU cores per task
+#SBATCH --mem=128g                   # 64GB memory allocation
 #SBATCH --partition=rleap_cpu       # Partition (queue) to use
 #SBATCH --output=output.txt  # Output log file
 #SBATCH --error=error.err
@@ -13,8 +13,8 @@
 echo "Running Python script in container using exec..."
 apptainer exec \
     --bind .:/work/rleap1/aaditya_mehta/Sketches--IW1_temporary \
-    mycontainer.sif \
-    sh -c "cd src && make clean && python3 running.py /work/rleap1/aaditya_mehta/Sketches--IW1_temporary/Adventure.bin 10000 5000000 --compile-first --output-file=output.txt"
+    mycontainer2.sif \
+    sh -c "cd src && make clean && echo 32 128 10000 500000 >> output.txt && python3 running.py /work/rleap1/aaditya_mehta/Sketches--IW1_temporary/Adventure.bin 10000 500000 --compile-first --output-file=output.txt"
 
 # Check if the execution was successful
 if [ $? -eq 0 ]; then
---
+++ my_slurm2.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --nodes=1                   # Number of nodes
+#SBATCH --ntasks-per-node=1         # Number of tasks per node
+#SBATCH --cpus-per-task=32          # 16 CPU cores per task
+#SBATCH --mem=128g                   # 64GB memory allocation
+#SBATCH --partition=rleap_cpu       # Partition (queue) to use
+#SBATCH --output=output2.txt  # Output log file
+#SBATCH --error=error2.err
+
+
+
+# Run the Python script inside the container using exec with proper directory navigation
+echo "Running Python script in container using exec..."
+apptainer exec \
+    --bind .:/work/rleap1/aaditya_mehta/Sketches--IW1_temporary \
+    mycontainer2.sif \
+    sh -c "cd src && make clean && echo cpu 32 138 50000 700000 >> output2.txt && python3 running.py /work/rleap1/aaditya_mehta/Sketches--IW1_temporary/Adventure.bin 50000 700000 --compile-first --output-file=output2.txt"
+
+# Check if the execution was successful
+if [ $? -eq 0 ]; then
+    echo "Container execution completed successfully!"
+else
+    echo "Container execution failed."
+    exit 1
+fi
+
+echo "Script completed successfully!"
