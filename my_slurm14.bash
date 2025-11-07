cb933ad071a5b904a95e1e4952d50cc4e72c50ac
root <root@AadisLaptop.>
2025-11-07 15:39:06 +0100

    modifed slurms

--- my_slurm14.bash
+++ my_slurm14.bash
@@ -14,7 +14,7 @@ echo "Running Python script in container using exec..."
 apptainer exec \
     --bind .:/work/rleap1/aaditya_mehta/Sketches--IW1_temporary \
     mycontainer2.sif \
-      sh -c "cd src && make clean && echo 32 128 50000 time 10 000 000 sim max_execution_length 75000 lookahead caching 2 max_rep 30000 max depth 900 000>> output14.txt && python3 running.py /work/rleap1/aaditya_mehta/Sketches--IW1_temporary/Adventure.bin 50000 10000000 --additional-args='--nodes-threshold 200000000 --max-depth 900000 --max-execution-length 75000 --lookahead_depth 10000 --max-rep 30000 --lookahead-caching 2' --compile-first --output-file=output14.txt"
+      sh -c "cd src && make clean  && make && echo 32 128 50000 time 10 000 000 sim max_execution_length 75000 lookahead caching 2 max_rep 30000 max depth 900 000>> output14.txt && python3 running.py /work/rleap1/aaditya_mehta/Sketches--IW1_temporary/Adventure.bin 50000 10000000 --additional-args='--nodes-threshold 200000000 --max-depth 900000 --max-execution-length 75000 --lookahead_depth 10000 --max-rep 30000 --lookahead-caching 2' --compile-first --output-file=output14.txt"
 
 # Check if the execution was successful
 if [ $? -eq 0 ]; then
